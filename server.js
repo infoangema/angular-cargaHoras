645c3779f52ce74efb6b3e9d23b03788bbda1672
paletgerardo <paletgerardo@hotmail.com>
2021-07-29 13:32:51 -0300

    version 0.0.1

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-//Install express server
-const express = require('express');
 const path = require('path');
-
+const express = require('express');
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-cargaHoras'));
+// Serve static files
+app.use(express.static(__dirname + '/dist/angular-cargaHoras'));
 
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/angular-cargaHoras/'}),
-);
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/angular-cargaHoras/index.html'));
+});
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// default Heroku port
+app.listen(process.env.PORT || 5000);
