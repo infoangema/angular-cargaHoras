367bb85549af00cf9143a41054de1a007b12c9be
paletgerardo <paletgerardo@hotmail.com>
2021-08-07 17:09:29 -0300

    version 0.0.4

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/dist/angularcargahoras'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/angularcargahoras/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/angularcargahoras/src/index.html'));
 });
 
 // default Heroku port
