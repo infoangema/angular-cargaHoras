5bb9caef21bcbeb31b75bdbb96c21b35161fbc21
paletgerardo <paletgerardo@hotmail.com>
2021-08-07 17:35:40 -0300

    version 0.0.9

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static("./dist/angularcargahoras"));
+app.use (express.static (__dirname + '/angularcargahoras'));
 
 // Send all requests to index.html
-app.get("/*", function (req, res) {
-  res.sendFile("index.html", { root: "dist/angularcargahoras" });
+app.get ('/*', function (req, res) {
+  res.sendFile (path.join (__dirname, 'angularcargahoras', 'index.html'))
 });
 
 // default Heroku port
