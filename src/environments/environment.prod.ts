2fa4897e010727d3fc274de27d9bf34529cc979b
infoangema <info@angema.com.ar>
2023-04-20 18:52:43 -0300

    Feat: integracion con spring.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  URL_BASE: 'https://hours-backend-pruebas.herokuapp.com/auth'
+  URL_BASE: 'https://hours-backend-pruebas.herokuapp.com/auth',
+  httpTimeout: 15000,
+  retry: 0
 };
