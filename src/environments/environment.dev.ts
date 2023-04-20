de8c1c533a6ddbe047fadd5eecf2047d582fa7b3
infoangema <info@angema.com.ar>
2023-04-19 21:04:14 -0300

    Feat: completado respuesta global en todos los endpoints y dto en todos los endpoints

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: false,
   URL_BASE: 'http://localhost:8082',
-//  url: 'https://jsonplaceholder.typicode.com',
   httpTimeout: 3000,
   retry: 2
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  URL_BASE: 'https://hours-backend-pruebas.herokuapp.com/auth'
-};
+  URL_BASE: 'http://200.114.219.199',
+  httpTimeout: 3000,
+  retry: 2
+}
