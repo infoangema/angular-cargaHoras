efdac279bcf4981823f7ecaa8553d2f4fea2423a
infoangema <info@angema.com.ar>
2023-04-21 11:35:43 -0300

    Code: Set environment.prod.ts url server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  URL_BASE: 'https://hours-backend-pruebas.herokuapp.com/auth',
+  URL_BASE: 'https://200.114.219.199/auth',
   httpTimeout: 15000,
   retry: 0
 };
