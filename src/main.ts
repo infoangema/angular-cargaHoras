08f20bb822538e9dbab9b92eba74fe85d7fd088a
gerrdevs <paletgerardo.devs@gmail.com>
2023-04-29 22:57:25 -0300

    Conf: proxy 3

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-// Habilita la seguridad de contenido
-import 'web-animations-js';
-import 'zone.js/dist/zone';
-import 'core-js/es/reflect';
-import 'core-js/stable/reflect';
-import 'core-js/features/reflect';
-import 'classlist.js';
-import 'web-animations-js';
-
 if (environment.production) {
   enableProdMode();
 }
